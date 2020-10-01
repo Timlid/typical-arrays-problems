82e1e9f95c7806839841372e9098e994736c9663
Temurjon Sharifov <temurjon.sharifov@gmail.com>
2020-10-01 21:03:42 +0500

    feat: add average function

--- src/index.js
+++ src/index.js
@@ -17,5 +17,13 @@ exports.max = function max(array) {
 };
 
 exports.avg = function avg(array) {
-    return 0;
+    if (array == "" || array == undefined) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
 };

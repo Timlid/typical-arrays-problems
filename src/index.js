c00a2b2f727b223e8b444a647ab807220bd2830b
Temurjon Sharifov <temurjon.sharifov@gmail.com>
2020-10-01 20:48:22 +0500

    init: start 'Typical Array Problems' task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+// ! TYPICAL ARRAY PROBLEMS
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return 0;
+};
